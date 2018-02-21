2e1bf75a1ec60193620f3a04d6c83522cdf98366
Sakeed <36423362+Sakeed@users.noreply.github.com>
2018-02-21 20:40:03 +0100

    RSVP

--- Proyecto/Acme-Rendezvous/src/main/java/controllers/user/RSVPUserController.java
+++ Proyecto/Acme-Rendezvous/src/main/java/controllers/user/RSVPUserController.java
@@ -3,12 +3,14 @@ package controllers.user;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.Assert;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import domain.RSVP;
+import domain.Rendezvous;
 import domain.User;
 
 import services.RSVPService;
@@ -36,6 +38,34 @@ public class RSVPUserController {
 		return result;
 	}
 	
+	
+	@RequestMapping(value = "/edit", method = RequestMethod.POST, params = "delete")
+	public ModelAndView delete(final RSVP rsvp, final BindingResult binding) {
+//		ModelAndView res;
+//		Assert.notNull(rsvpId);
+//		RSVP rsvp = this.rsvpService.findOne(rsvpId);
+//		this.rsvpService.delete(rsvp);
+//		res = new ModelAndView("redirect:list.do");
+//		return res;
+		
+
+		ModelAndView result = null;
+
+		try {
+
+			this.rsvpService.delete(rsvp);
+			result = new ModelAndView("redirect:../list.do");
+
+		}
+
+		catch (final Throwable oops) {
+			if (rsvp.getId()==0)
+				result = this.createEditModelAndView(rsvp, "category.comit.error2");
+			
+		}
+		return result;
+
+	}
 	private ModelAndView createEditModelAndView(final RSVP rsvp) {
 		final ModelAndView res = this.createEditModelAndView(rsvp, null);
 		return res;
@@ -53,7 +83,7 @@ public class RSVPUserController {
 	private ModelAndView createEditModelAndView(final RSVP rsvp, final String message) {
 		final ModelAndView res;
 
-		res = new ModelAndView("rsvp/create");
+		res = new ModelAndView("rsvp/user/edit");
 		res.addObject("RSVP", rsvp);
 		res.addObject("message", message);
 
