855be8d741cc893dc874839463c485dbb241153d
Silverath <pabvazzam@gmail.com>
2018-02-26 00:58:25 +0100

    .

--- Proyecto/Acme-Rendezvous/src/main/java/services/QuestionService.java
+++ Proyecto/Acme-Rendezvous/src/main/java/services/QuestionService.java
@@ -25,16 +25,12 @@ public class QuestionService {
 	// Managed repository -----------------------------------------------------
 
 	@Autowired
-<<<<<<< HEAD
 	private QuestionRepository		questionRepository;
-=======
-	private QuestionRepository			questionRepository;
->>>>>>> c25f182381200f18a6cd17075b670a7471dba377
 
 	@Autowired
 	private RendezvousService		rendezvousService;
 	@Autowired
-<<<<<<< HEAD
+
 	private UserService				userService;
 
 	@Autowired
@@ -42,12 +38,6 @@ public class QuestionService {
 
 	@Autowired
 	private Validator				validator;
-=======
-	private UserService		userService;
-
-	@Autowired
-	private AdministratorService		administratorService;
->>>>>>> c25f182381200f18a6cd17075b670a7471dba377
 
 
 	// Constructors -----------------------------------------------------------
@@ -58,12 +48,8 @@ public class QuestionService {
 
 	// Simple CRUD methods ----------------------------------------------------
 
-<<<<<<< HEAD
 	public Question create(final int rendezvousId) {
-=======
 
-	public Question create(final int rendezvousId){
->>>>>>> c25f182381200f18a6cd17075b670a7471dba377
 		Question result;
 		result = new Question();
 		final Rendezvous rendezvous = this.rendezvousService.findOne(rendezvousId);
@@ -77,23 +63,14 @@ public class QuestionService {
 		return result;
 	}
 
-<<<<<<< HEAD
 	public Question findOne(final int questionId) {
 		final Question question = this.questionRepository.findOne(questionId);
 		Assert.isTrue(question.getId() != 0);
 		return question;
 	}
 
-	public Question save(final Question question) {
-=======
-	public Question findOne(final int questionId){
-		final Question question = this.questionRepository.findOne(questionId);
-		Assert.isTrue(question.getId()!=0);
-		return question;
-	}
 
 	public Question save(final Question question){
->>>>>>> c25f182381200f18a6cd17075b670a7471dba377
 
 		final Question saved = this.questionRepository.save(question);
 		return saved;
@@ -102,30 +79,25 @@ public class QuestionService {
 	public void deleteByAdmin(final Question question) {
 
 		Assert.notNull(question);
-<<<<<<< HEAD
+
 		final Administrator administrator = this.administratorService.findByPrincipal();
 		Assert.notNull(administrator);
 		this.questionRepository.delete(question);
 	}
 
 	public Collection<Question> findAllByPrincipalAndRendezvous(final int principalId, final int rendezvousId) {
-=======
 
-		final Administrator administrator = this.administratorService.findByPrincipal();
-		Assert.notNull(administrator);
-
-		this.questionRepository.delete(question);
-	}
-
-	public Collection<Question> findAllByPrincipalAndRendezvous(final int principalId, final int rendezvousId){
->>>>>>> c25f182381200f18a6cd17075b670a7471dba377
-		return this.questionRepository.findAllByPrincipalAndRendezvous(principalId, rendezvousId);
+		Collection<Question> questions = new ArrayList<Question>();
+		questions = this.questionRepository.findAllByPrincipalAndRendezvous(principalId, rendezvousId);
+		final User user = this.userService.findByPrincipal();
+		Assert.notNull(user);
+		return questions;
 	}
 
 	public Collection<Question> findAllByrendezvous(final int rendezvousId) {
 		return this.questionRepository.findAllByRendezvous(rendezvousId);
 	}
-<<<<<<< HEAD
+
 	//Prune domain object------------------------------------------------------------
 	public Question reconstruct(final Question question, final BindingResult binding) {
 		Question res;
@@ -138,8 +110,5 @@ public class QuestionService {
 		}
 		return res;
 	}
-=======
-
->>>>>>> c25f182381200f18a6cd17075b670a7471dba377
 
 }
