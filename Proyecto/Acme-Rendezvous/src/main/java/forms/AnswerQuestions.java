5fb27c429da7ea104f7ce4f2b1607a2c0778e8e1
Silverath <pabvazzam@gmail.com>
2018-02-26 02:24:40 +0100

    .

--- Proyecto/Acme-Rendezvous/src/main/java/forms/AnswerQuestions.java
+++ Proyecto/Acme-Rendezvous/src/main/java/forms/AnswerQuestions.java
@@ -2,6 +2,11 @@ package forms;
 
 import java.util.Collection;
 
+import javax.persistence.ElementCollection;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
 import domain.Question;
 
 public class AnswerQuestions {
@@ -14,12 +19,15 @@ public class AnswerQuestions {
 	private Collection<Question>	questions;
 	private Collection<String>	answers;
 	
+	@ElementCollection
 	public Collection<Question> getQuestions() {
 		return questions;
 	}
 	public void setQuestions(Collection<Question> questions) {
 		this.questions = questions;
 	}
+	@NotEmpty
+	@ElementCollection
 	public Collection<String> getAnswers() {
 		return answers;
 	}
