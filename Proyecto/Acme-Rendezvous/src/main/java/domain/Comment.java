eef46e129a850f30bb0008231c135a1576f3bbde
javierelena94 <javierelena94@gmail.com>
2018-02-26 01:17:03 +0100

    Índices desde Administrator hasta Comment

--- Proyecto/Acme-Rendezvous/src/main/java/domain/Comment.java
+++ Proyecto/Acme-Rendezvous/src/main/java/domain/Comment.java
@@ -8,7 +8,9 @@ import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.Index;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
@@ -20,6 +22,9 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 @Access(AccessType.PROPERTY)
+@Table(indexes = {
+	@Index(columnList = "moment")
+})
 public class Comment extends DomainEntity {
 
 	private Date				moment;
